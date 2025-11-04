f24fd5fe3c005c482897d7d75c9d191b2edf2a22
Oksana_Melnyk <kc.19870625@gmail.com>
2025-11-04 21:18:25 +0200

    update file app/notes/filter/(.)notes/[id]/page.tsx

--- app/notes/filter/(.)notes/[id]/page.tsx
+++ app/notes/filter/(.)notes/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api'; 
 import NotePreview from '@/components/NotePreview/NotePreview';
-import Modal from '@/components/Modal/Modal';
 import { redirect } from 'next/navigation';
 
 interface InterceptedNotePageProps {
@@ -31,22 +30,4 @@ export default async function InterceptedNotePage({ params }: InterceptedNotePag
   );
 }
 
-'use client';
-import { useQuery } from '@tanstack/react-query';
-import Modal from '../Modal/Modal'; 
-import { useRouter } from 'next/navigation';
 
-
-const NotePreview = ({ noteId }: { noteId: string }) => {
-    const router = useRouter();
-    
-    const handleClose = () => {
-        router.back(); 
-    };
-
-    return (
-        <Modal onClose={handleClose}>
-        </Modal>
-    );
-};
-*/
