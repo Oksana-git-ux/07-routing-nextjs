36872eafe455e9af196bb8f481f566b84f093684
Oksana_Melnyk <kc.19870625@gmail.com>
2025-11-06 11:46:10 +0200

    update file Notes.client.tsx

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,72 +1,25 @@
 'use client';
 
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import toast, { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 import dynamic from 'next/dynamic';
 
-import { fetchNoteById } from '@/lib/api';
 import Modal from '@/components/Modal/Modal';
 import css from '@/app/notes/Notes.module.css';
-
-const DynamicErrorMessage = dynamic(
-  () => import('@/components/ErrorMessage/ErrorMessage'),
-  { ssr: false }
-);
-
-const DynamicLoader = dynamic(() => import('@/components/Loader/Loader'), {
-  ssr: false,
-});
 
 const NotePreviewComponent = dynamic(
   () => import('@/components/NotePreview/NotePreview'),
   { ssr: false }
 );
 
-interface NotePreviewClientProps {
-  id: string;
-}
-
-const NotePreviewClient: React.FC<NotePreviewClientProps> = ({ id }) => {
-  const {
-    data: note,
-    isError,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ['note', id],
-    queryFn: async ({ queryKey }) => {
-      const noteId = queryKey[1] as string;
-      return fetchNoteById(noteId);
-    },
-    staleTime: 5 * 60 * 1000,
-  });
-
-  React.useEffect(() => {
-    if (isError) {
-      const message =
-        error instanceof Error ? error.message : 'Failed to load note.';
-      toast.error(`Error loading note: ${message}`);
-    }
-  }, [isError, error]);
+const NotePreviewClient: React.FC<{ id: string }> = ({ id }) => {
+  const router = useRouter();
 
   return (
-    <Modal onClose={() => window.history.back()}>
+    <Modal onClose={() => router.back()}>
       <div className={css.notePreviewWrapper}>
-        {isLoading && <DynamicLoader />}
-
-        {isError && (
-          <DynamicErrorMessage
-            message={
-              error instanceof Error ? error.message : 'Error loading note.'
-            }
-          />
-        )}
-
-        {id && <NotePreviewComponent noteId={id} />}
+        <NotePreviewComponent noteId={id} />
       </div>
-
-      <Toaster position="top-right" reverseOrder={false} />
     </Modal>
   );
 };
